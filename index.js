246c0c7f7bbd2d6e0339c766b1495e6d032b658d
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-08-14 14:39:28 -0700

    First pass

---
+++ index.js
@@ -0,0 +1,45 @@
+class Prom {
+  constructor (cb) {
+    this.resolve = this.resolve.bind(this)
+    this.reject = this.reject.bind(this)
+
+    this.resolved = false
+    this.rejected = false
+    this.resolvedData = undefined
+    this.rejectedData = undefined
+
+    cb(this.resolve, this.reject)
+  }
+  resolve (data) {
+    this.resolved = true
+    this.resolvedData = data
+    if (this.resolver) {
+      this.resolver(data)
+    }
+  }
+  reject (data) {
+    this.rejected = true
+    this.rejectedData = data
+    if (this.rejecter) {
+      this.rejecter(data)
+    }
+  }
+  then (resolve = () => {}, reject = () => {}) {
+    this.resolver = resolve
+    this.rejecter = reject
+    if (this.resolved) {
+      this.resolver(this.resolvedData)
+    }
+    if (this.rejected) {
+      this.rejecter(this.rejectedData)
+    }
+  }
+  catch (reject = () => {}) {
+    this.rejecter = reject
+    if (this.rejected) {
+      this.rejecter(this.rejectedData)
+    }
+  }
+}
+
+module.exports = Prom
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const Prom = require('.')
 
 function a () {
@@ -15,11 +16,27 @@ function b () {
 }
 
 async function main () {
+  console.log('Promise style tests:')
+  a().then((res) => {
+    console.log('got this from a:', res)
+    assert(res, 'done with a')
+  })
+  b().catch((e) => {
+    console.log('got this from b:', e)
+    assert(e, 'done with b')
+  })
+
+  console.log('\n\n')
+
+  console.log('Async/await style tests:')
   const ares = await a()
+  console.log('got this from a:', ares)
+  assert.strictEqual(ares, 'done with a')
   try {
     await b()
   } catch (e) {
     console.log('got this from b:', e)
+    assert.strictEqual(e, 'done with b')
   }
 }
 
